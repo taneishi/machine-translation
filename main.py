e8b33e5fb0c189ce4409639150e6875fc0390154
taneishi <taneishi@users.noreply.github.com>
2020-05-26 15:47:25 +0900

    remove tqdm

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import torch
 import torch.optim as optim
 import torch.nn as nn
-import torchtext
 from torchtext.data import Field, BucketIterator, TabularDataset
 import sentencepiece as spm
-from tqdm import tqdm
 import random
 import math
-import sys
-import os
 
 from model import Encoder, Decoder, Seq2Seq
 
-def train(model, train_it, optimizer, criterion, clip):
+sp = spm.SentencePieceProcessor()
+sp.Load('model/ja.wiki.bpe.vs5000.model')
+
+def train(model, train_it, optimizer, criterion, clip, epoch):
     model.train()
-    epoch_loss = 0
-    for i, batch in tqdm(enumerate(train_it)):
+    train_loss = 0
+    for index, batch in enumerate(train_it, 1):
         src = batch.jp
         trg = batch.en
         optimizer.zero_grad()
@@ -24,21 +23,34 @@ def train(model, train_it, optimizer, criterion, clip):
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
         optimizer.step()
-        epoch_loss += loss.item()
-    return epoch_loss/ len(train_it)
+        train_loss += loss.item()
+        print('\repoch %03d batch %03d train_loss %6.3f' % (epoch, index, train_loss / index), end='')
+    return train_loss / len(train_it)
 
-def evaluate(model, data_it, criterion):
+def test(model, data_it, criterion):
     model.eval()
-    epoch_loss = 0
-    for i, batch in tqdm(enumerate(data_it)):
+    test_loss = 0
+    for index, batch in enumerate(data_it, 1):
         src = batch.jp
         trg = batch.en
         output = model(src, trg, 0)
         loss = criterion(output[1:].view(-1, output.shape[-1]), trg[1:].view(-1))
-        epoch_loss += loss.item()
-    return epoch_loss/ len(data_it)
+        test_loss += loss.item()
+    print(' test_loss %6.3f' % (test_loss / index), end='')
+    return test_loss / len(data_it)
 
-def translate_sentence(sentence):
+def tokenize_jp(x):
+    x = str(x).lower()
+    # if want to reverse the order of the sentences
+    # return sp.EncodeAsPieces(x)[::-1]
+    return sp.EncodeAsPieces(x)
+
+def tokenize_en(x):
+    x = str(x).lower()
+    x = x.translate({ord(c): None for c in '!.?,'})
+    return x.split()
+
+def translate_sentence(sentence, SRC, TRG, model, device):
     tokenized = tokenize_jp(sentence)
     numericalised = [SRC.vocab.stoi[t] for t in tokenized]
     tensor = torch.LongTensor(numericalised).unsqueeze(1).to(device)
@@ -48,20 +60,6 @@ def translate_sentence(sentence):
     return translation
 
 def main():
-    sp = spm.SentencePieceProcessor()
-    sp.Load('data/ja.wiki.bpe.vs5000.model')
-
-    def tokenize_jp(x):
-        x = str(x).lower()
-        #if want to reverse the order of the sentences
-        #return sp.EncodeAsPieces(x)[::-1]
-        return sp.EncodeAsPieces(x)
-
-    def tokenize_en(x):
-        x = str(x).lower()
-        x = x.translate({ord(c): None for c in '!.?,'})
-        return x.split()
-
     SRC = Field(tokenize=tokenize_jp, init_token='<sos>', eos_token='<eos>')
     TRG = Field(tokenize=tokenize_en, init_token='<sos>', eos_token='<eos>')
 
@@ -79,22 +77,16 @@ def main():
     #print ('index 3 in the trg:', TRG.vocab.itos[3])
     #print ('index the in the trg:', TRG.vocab.stoi['the'])
 
-    bsize = 32
+    batch_size = 32
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     train_it, valid_it, test_it = BucketIterator.splits(
             (train_dt, valid_dt, test_dt), 
-            batch_size=bsize, 
+            batch_size=batch_size, 
             sort_key=lambda x: len(x.jp), 
             sort_within_batch=False, 
             device=device)
-
-    '''
-    for b in train_it:
-        print (b.jp, b.en)
-        sys.exit()
-    '''
 
     input_dim = len(SRC.vocab)
     out_dim = len(TRG.vocab)
@@ -114,35 +106,36 @@ def main():
     pad_idx = TRG.vocab.stoi['<pad>']
     criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
 
-    epoch = 10
+    epochs = 30
     clip = 1
-    savedir = 'models'
-    model_save_path = os.path.join(savedir, 's2smodel.pt')
+    model_save_path = 'model/s2smodel.pt'
     best_valid_loss = float('inf')
 
-    os.makedirs(f'{savedir}', exists_ok=True)
-    for ep in range(epoch):
-        train_loss = train(model, train_it, optimizer, criterion, clip)
-        valid_loss = evaluate(model, valid_it, criterion)
+    for epoch in range(epochs):
+        train_loss = train(model, train_it, optimizer, criterion, clip, epoch)
+        valid_loss = test(model, valid_it, criterion)
         
         if valid_loss < best_valid_loss:
             best_valid_loss = valid_loss
             torch.save(model.state_dict(), model_save_path)
         
-        print(f'epoch: {ep+1:03} | train loss: {train_loss: .3f} | train_ppl: {math.exp(train_loss):7.3f} | Val. Loss: {valid_loss:.3f} | Val. PPL: {math.exp(valid_loss):7.3f} |')
+        print(' train_ppl %7.3f test_ppl %7.3f' % \
+                (math.exp(train_loss), math.exp(valid_loss)))
+
+        candidate = ' '.join(vars(valid_dt.examples[2])['jp'])
+        print(translate_sentence(candidate, SRC, TRG, model, device))
 
     model.load_state_dict(torch.load(model_save_path))
-    test_loss = evaluate(model, test_it, criterion)
+    test_loss = test(model, test_it, criterion)
 
-    print(f'|test loss: {test_loss: .3f} | test_ppl: {math.exp(test_loss):7.3f}|')
+    print(f' test_loss {test_loss: .3f} test_ppl {math.exp(test_loss):7.3f}')
 
     candidate = ' '.join(vars(valid_dt.examples[2])['jp'])
-    #candidate = '私はリンゴが好きです'
     candidate_translation = ' '.join(vars(valid_dt.examples[2])['en'])
 
     print(candidate)
     print(candidate_translation)
-    print(translate_sentence(candidate))
+    print(translate_sentence(candidate, SRC, TRG, model, device))
 
     tokenized = tokenize_jp(candidate)
     numericalised = [SRC.vocab.stoi[t] for t in tokenized]
